3db6baf41723f24aabb0604fd2549c6657b5769f
Aishah <aishah92@gmail.com>
2021-01-24 01:28:55 +0000

    Add hard-coded fetch functionality

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { getData, API_BASE } from './../utils/dataHelper';
 
 const Homepage = () => {
-  const [orgName, setOrgName] = React.useState();
+  const [orgName, setOrgName] = React.useState('elsewhencode');
   const [reposList, setReposList] = React.useState();
 
   React.useEffect(() => {
+    const url = `${API_BASE}/orgs/${orgName}/repos`;
+    getData(url).then((repos) => setReposList(repos));
+
     
 
   }, [])
+
+  
+
+  
+  
 
   return (
     <main>
       <input type='text' placeholder='Type a Github organisation here...' value={orgName} onChange={(e) => setOrgName(e.target.value)} />
       <button>Hey Fetcho!</button>
+      <h2>List of {orgName}'s Repositories</h2>
       
     </main>
   )
